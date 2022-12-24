3a2a1409e50b1afc87712a5738021da3943b5dc2
tech-savvypal <120805883+tech-savvypal@users.noreply.github.com>
2022-12-24 21:35:10 +0530

    Add files via upload

---
+++ Project2_task2_range.py
@@ -0,0 +1,6 @@
+start=int(input("Enter the starting value of the range:"))
+end=int(input("Enter the ending value of the range:"))
+for i in range(start,end+1):
+    if i>0:
+        print(i)
+        
